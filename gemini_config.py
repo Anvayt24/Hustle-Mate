c61a70b295891134e532a49e4b692833d2c7cafa
anvay <anvatrive2324@gmail.com>
2025-07-11 21:13:47 +0530

    tried a different gemini model

--- gemini_config.py
+++ gemini_config.py
@@ -6,7 +6,8 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 def call_gemini(prompt: str) -> str:
-    model = genai.GenerativeModel("gemini-2.5-flash")
+    model = genai.GenerativeModel("gemini-2.5-flash") #model to 2.5-flash
+    
     response = model.generate_content(prompt)
     return response.text
 
